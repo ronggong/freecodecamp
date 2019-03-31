be701d3d6db4d06aa9711ca29042bbf54f64d0f0
ronggong <ronggong1010@gmail.com>
2019-03-31 22:17:10 +0100

    implemented data analyse in server.js

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+var multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 require("dotenv").config();
 
 const app = express();
 app.use(cors());
 
 app.use(express.static(__dirname + '/views'));
+
+app.post('/api/fileanalyse', upload.single("upfile"), (req, res)=>{
+    const file = req.file;
+    res.json({name: file.originalname, type: file.mimetype, size: file.size});
+});
 
 const listener = app.listen(process.env.PORT, ()=>{
     console.log(`Your app is listening on port ${listener.address().port}`);
